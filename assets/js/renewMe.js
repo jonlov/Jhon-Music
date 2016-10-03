f3bba0351f065c6474ff67ce5a8c45869eae47e6
Jonathan Lovera <jonlov@192-168-1-8.tpgi.com.au>
2016-10-03 18:01:05 +1100

    Fixed css style, there was less inside the css file.

---
+++ assets/js/renewMe.js
@@ -0,0 +1,2 @@
+if (window.location.hostname == 'localhost')
+    document.write('<script src="http://' + window.location.hostname + ':35729/livereload.js?snipver=1" type="text/javascript"><\/script>');
