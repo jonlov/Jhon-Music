0583c6441aa51ebbd20433192e5153e850153d6d
Jonathan Lovera <jonlov@192-168-1-8.tpgi.com.au>
2016-10-04 19:10:50 +1100

    Sections almost done

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+angular
+    .module('app', [
+        'ngAnimate',
+        'ngCookies',
+        'ngResource',
+        'ngSanitize',
+        'ngTouch',
+        'ui.router',
+        'ui.bootstrap',
+        'oc.lazyLoad'
+    ]);
