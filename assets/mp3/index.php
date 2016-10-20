cf8da5cdeff4356ee15841e013cfc2151314b646
Jonathan Lovera <jonlov@192-168-1-3.tpgi.com.au>
2016-10-21 00:06:49 +1100

    Fixed download mp3

--- assets/mp3/index.php
+++ assets/mp3/index.php
@@ -13,25 +13,30 @@ if($_GET['file']){
     $shortlen = $fsize-1;
   }
 
-  if (file_exists($file)) {
-      header("Pragma: public");
-      header("Expires: 0"); 
-      header("Content-Type: audio/mpeg");
-      header('Content-Length: ' . $fsize);
-      header('Content-Disposition: inline; filename="' . basename($file) . '"');
-      header( 'Content-Range: bytes 0-'.$shortlen.'/'.$fsize); 
-      header( 'Accept-Ranges: bytes');
-      header('X-Pad: avoid browser bug');
-      header('Cache-Control: no-cache');
-      header('Etag: ' . $etag);
+  if($_GET['download'] == true){
+      header('Content-Description: File Transfer');
+      header('Content-Type: application/octet-stream');
+      header('Content-Disposition: attachment; filename='.basename($file));
+      header('Content-Transfer-Encoding: binary');
+      header('Expires: 0');
+      header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+      header('Pragma: public');
 
-      // header('Content-Description: File Transfer');
-      // header('Content-Type: application/octet-stream');
-      // header('Content-Disposition: attachment; filename='.basename($file));
-      // header('Content-Transfer-Encoding: binary');
-      // header('Expires: 0');
-      // header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-      // header('Pragma: public');
+  } else {
+    header("Pragma: public");
+    header("Expires: 0"); 
+    header("Content-Type: audio/mpeg");
+    header('Content-Length: ' . $fsize);
+    header('Content-Disposition: inline; filename="' . basename($file) . '"');
+    header( 'Content-Range: bytes 0-'.$shortlen.'/'.$fsize); 
+    header( 'Accept-Ranges: bytes');
+    header('X-Pad: avoid browser bug');
+    header('Cache-Control: no-cache');
+    header('Etag: ' . $etag);
+    
+  }
+
+  if (file_exists($file)) {
       ob_clean();
       flush();
       readfile($file);
