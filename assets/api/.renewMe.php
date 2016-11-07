56a1277959396286cbcaca1fccd40b897fbf4606
Jonathan Lovera <rockjonathan18@gmail.com>
2016-11-07 06:50:51 +0000

    Update .renewMe.php upload the wrong one again

--- assets/api/.renewMe.php
+++ assets/api/.renewMe.php
@@ -2,17 +2,21 @@
 	function getDomain(){
 		return '@@renewDomain';
 	}
+
  	header("Access-Control-Allow-Origin: ".getDomain());
 	header("Access-Control-Allow-Credentials: true");
 	date_default_timezone_set('UTC');
+
 	function openRead($fileName = ''){
 		if(is_file($fileName) && filesize($fileName) > 0){
 			$file = fopen($fileName, "r");
 			$data = fread($file, filesize($fileName));
 			fclose($file);
 		} else $data = null;
+
 		return $data;
 	}
+
 	function throwError($message, $statusCode = '400'){
 		if($statusCode == 404){
 			$message = '<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
@@ -27,6 +31,7 @@
 			
 		}	
 		print_r($_SERVER);
+
 		header('HTTP/1.0 '.$statusCode);
 		die($message);
 	}
@@ -44,24 +49,30 @@
 	//     rmdir($dir);
 	//   }
  // 	}
+
 	function encrypt_decrypt($action, $string) {
 	    $output = false;
+
 	    $encrypt_method = "AES-256-CBC";
 	    $secret_key = 'This is my secret key';
 	    $secret_iv = 'This is my secret iv';
+
 	    // hash
 	    $key = hash('sha256', $secret_key);
 	    
 	    // iv - encrypt method AES-256-CBC expects 16 bytes - else you will get a warning
 	    $iv = substr(hash('sha256', $secret_iv), 0, 16);
+
 	    if( $action == 'encrypt' ) {
 	        $output = openssl_encrypt($string, $encrypt_method, $key, 0, $iv);
 	        $output = base64_encode($output);
 	    } else if( $action == 'decrypt' ){
 	        $output = openssl_decrypt(base64_decode($string), $encrypt_method, $key, 0, $iv);
 	    }
+
 	    return $output;
 	}
+
 	// if($_GET['delete'] && $_GET['token']){
 	// 	$token = $_GET['token'];
 	// 	echo $token;
@@ -72,33 +83,45 @@
 	// 	// 	else 
 	// 	// 		rrmdir($file);
 	// 	// }
+
 	// } else {
 	// $res = true;
+
 	function twoMoreHours($fileName, $date, $gitID){
 		$expDate = $date + (2 * 3600);
+
 		$fileW = fopen($fileName, "w");
 		$encrypted = encrypt_decrypt('encrypt', $expDate);
 		fwrite($fileW, $encrypted.'.'.base64_encode($gitID).'$'.$expDate);
+
 		fclose($fileW);
 		chmod($fileName, 0644);
 		die();
 	}
+
 	function destroyRenewMe($fileName){
 	  	if(is_file($fileName))
 	    	unlink($fileName); // delete file
 		die(true);
 	}
+
 	$fileName = '.renewMe';
 	$date = getdate();
 	$date = $date[0];
+
 	if($_GET['showMe']) {
 		$index = openRead('index.php');
 		$renewMe = openRead('.renewMe.php');
+
 		// die($renewMe . $index);
 		die(base64_encode($renewMe . $index));
+
 	} else if($_GET['deleteNow']) {
 		destroyRenewMe($fileName);
+
 	} else if($_GET['renewNow'] && $_GET['g']){
 		twoMoreHours($fileName, $date, $_GET['g']);
+
 	} else throwError(null, 404);
+
 ?>
